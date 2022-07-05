388b3765c56ab5ca1d5ffd3cdfe4577edcc65b64
王亚军 <immc.wang@biposervice.com>
2022-07-05 16:55:10 +0800

    refactor: 拆分循环

--- statement.js
+++ statement.js
@@ -1,47 +1,78 @@
-
 function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-    case 'tragedy':
-      thisAmount = getTragedyAmount(perf);
-      break;
-    case 'comedy':
-      thisAmount = getComedyAmount(perf);
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
-    }
-    if ('tragedy' === play.type) {
-      volumeCredits += getTragedyVolumeCredits(perf);
-    }
-    if ('comedy' === play.type) {
-      volumeCredits += getComedyVolumeCredits(perf)
-    }
-    result += `  ${play.name}: ${formatUSD(thisAmount)} (${ perf.audience } seats)\n`;
-    totalAmount += thisAmount;
-  }
-  result += `Amount owed is ${formatUSD(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += getPerformancesDetails(invoice, plays);
+  result += `Amount owed is ${formatUSD(getTotalAmount(invoice, plays))}\n`;
+  result += `You earned ${getVolumeCredits(invoice, plays)} credits\n`;
   return result;
 }
 
 module.exports = statement;
 
-function formatUSD (number) {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+function getPerformancesDetails(invoice, plays) {
+  let details = "";
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = getTragedyAmount(perf);
+        break;
+      case "comedy":
+        thisAmount = getComedyAmount(perf);
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    details += `  ${play.name}: ${formatUSD(thisAmount)} (${
+      perf.audience
+    } seats)\n`;
+  }
+  return details;
+}
+
+function getTotalAmount(invoice, plays) {
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = getTragedyAmount(perf);
+        break;
+      case "comedy":
+        thisAmount = getComedyAmount(perf);
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    totalAmount += thisAmount;
+  }
+  return totalAmount;
+}
+
+function getVolumeCredits(invoice, plays) {
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    if ("tragedy" === play.type) {
+      volumeCredits += getTragedyVolumeCredits(perf);
+    }
+    if ("comedy" === play.type) {
+      volumeCredits += getComedyVolumeCredits(perf);
+    }
+  }
+  return volumeCredits;
+}
+
+function formatUSD(number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
     minimumFractionDigits: 2,
   }).format(number / 100);
 }
 
-function getComedyVolumeCredits (perf) {
+function getComedyVolumeCredits(perf) {
   return Math.max(perf.audience - 30, 0) + Math.floor(perf.audience / 5);
 }
 
@@ -65,4 +96,3 @@ function getTragedyAmount(perf) {
   }
   return result;
 }
-
